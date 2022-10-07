ad11c87699e125afffb9acbf66de169cf62aac35
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-10-07 12:20:16 +0800

    Added dotnet test to workflow

---
+++ Tests/TestFunctionApp.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace TestFunctionApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
+    }
+}
